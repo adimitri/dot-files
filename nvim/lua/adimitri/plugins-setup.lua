5651b01de92f87b6660110213c52f739eca0f718
Alexander E. Dimitriyadi <alexander.dimitriyadi@gmail.com>
2022-12-05 09:41:20 -0500

    Lots of changes to do autocompletion and other IDE-like things

--- nvim/lua/adimitri/plugins-setup.lua
+++ nvim/lua/adimitri/plugins-setup.lua
@@ -62,6 +62,31 @@ return packer.startup(function(use)
     },
   })
 
+  -- autocompletion
+  use("hrsh7th/nvim-cmp") -- completion plugin
+  use("hrsh7th/cmp-buffer") -- source for text in buffer
+  use("hrsh7th/cmp-path") -- source for file system paths
+
+  -- snippets
+  use("L3MON4D3/LuaSnip") --snippet engine
+  use("saadparwaiz1/cmp_luasnip") -- for autocompletion
+  use("rafamadriz/friendly-snippets") -- useful snippets
+
+  -- managing lsp servers, linters, and formatters
+  use("williamboman/mason.nvim") -- managing lsp servers, linters, and formatters
+  use("williamboman/mason-lspconfig.nvim") -- interface between mason and lspconfig
+
+  -- configuring lsp servers
+  use("neovim/nvim-lspconfig") -- easily configure language servers
+  use("hrsh7th/cmp-nvim-lsp") -- for autocompletion
+  use({ "glepnir/lspsaga.nvim", branch = "main" }) -- enhanced lsp uis
+  use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file and update imports)
+  use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
+
+  -- formatting and linting
+  use("jose-elias-alvarez/null-ls.nvim") -- configure formatters and linters
+  use("jayp0521/mason-null-ls.nvim") -- interface between mason and null-ls
+
   -- treesitter configuration
   use({
     "nvim-treesitter/nvim-treesitter",
@@ -70,6 +95,10 @@ return packer.startup(function(use)
       ts_update()
     end,
   })
+
+  -- auto closing
+  use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc
+  use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
 
   -- git integration
   use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
