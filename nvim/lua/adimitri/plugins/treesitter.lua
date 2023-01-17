1d2e27b46502ba6d7bf2971c9ac21a13db1be0a7
Alexander E. Dimitriyadi <alex@hiallocate.com>
2023-01-16 22:14:26 -0500

    Some indenting fixes

--- nvim/lua/adimitri/plugins/treesitter.lua
+++ nvim/lua/adimitri/plugins/treesitter.lua
@@ -1,30 +1,29 @@
 local status, treesitter = pcall(require, "nvim-treesitter.configs")
 if not status then
-  return
+	return
 end
 
 treesitter.setup({
-  highlight = {
-    enable = true,
-  },
-  indent = { enable = true },
-  autotag = { enable = true },
-  ensure_installed = {
-    "ruby",
-    "rust",
-    "json",
-    "javascript",
-    "typescript",
-    "tsx",
-    "yaml",
-    "html",
-    "css",
-    "markdown",
-    "bash",
-    "lua",
-    "vim",
-    "dockerfile",
-    "gitignore",
-  },
-  auto_install = true,
+	highlight = {
+		enable = true,
+	},
+	indent = { enable = true },
+	autotag = { enable = true },
+	ensure_installed = {
+		"ruby",
+		"rust",
+		"json",
+		"javascript",
+		"typescript",
+		"tsx",
+		"yaml",
+		"html",
+		"css",
+		"markdown",
+		"bash",
+		"lua",
+		"vim",
+		"dockerfile",
+	},
+	auto_install = true,
 })
