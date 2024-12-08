54837c29785e7c7b7eb88373fcbcbec4722c634c
Alexander E. Dimitriyadi <adimitri@users.noreply.github.com>
2024-12-08 11:57:01 -0500

    Makes further progress in configuring files

--- nvim/lua/adimitri/lazy.lua
+++ nvim/lua/adimitri/lazy.lua
@@ -11,4 +11,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("adimitri.plugins")
+require("lazy").setup("adimitri.plugins", {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
+})
